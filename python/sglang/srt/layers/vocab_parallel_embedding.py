10be27cc75751f98765cf95f1ec1157390874d14
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-02-21 14:58:22 +0000

    only update padding_size in VocabParallelEmbedding if can't be divided evenly

--- python/sglang/srt/layers/vocab_parallel_embedding.py
+++ python/sglang/srt/layers/vocab_parallel_embedding.py
@@ -234,8 +234,11 @@ class VocabParallelEmbedding(torch.nn.Module):
             self.tp_size = 1
 
         self.num_embeddings = num_embeddings
-        self.padding_size = padding_size * get_tensor_model_parallel_world_size()
         self.org_vocab_size = org_num_embeddings or num_embeddings
+        if pad_vocab_size(self.org_vocab_size, padding_size) % self.tp_size != 0:
+            padding_size *= self.tp_size
+        self.padding_size = padding_size
+
         num_added_embeddings = num_embeddings - self.org_vocab_size
         self.use_presharded_weights = use_presharded_weights
         if use_presharded_weights:
