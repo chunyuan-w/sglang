47b5cd70f060a53585e9631a44c0e15651756738
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-04-23 16:05:03 +0000

    partial draft code for fused_moe

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -246,6 +246,78 @@ class DeepseekV2MoE(nn.Module):
                 self.shared_experts_is_fp8 = True
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        # TODO: what check is needed?
+        if (
+            self.n_shared_experts is not None
+            and self.gate.use_intel_amx_backend
+            and self.experts.use_intel_amx_backend
+            and self.shared_experts_gate_up_proj.use_intel_amx_backend
+            and self.shared_experts_down_proj.use_intel_amx_backend
+        ):
+            return self.forward_moe_fused_cpu(hidden_states)
+        else:
+            return self.forward_normal(hidden_states)
+
+    def forward_moe_fused_cpu(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        # num_tokens, hidden_dim = hidden_states.shape
+        # hidden_states = hidden_states.view(-1, hidden_dim)
+        # if self.gate_impl is None:
+        #     self.gate_impl = self.gate.forward
+        # # router_logits: (num_tokens, n_experts)
+        # router_logits = self.gate_impl(hidden_states)
+        
+        router_logits = sgl_kernel.cpu.forward_moe_fused(
+            hidden_states,
+            self.gate.weight,
+            None, #bias
+        )
+        # TODO: move select experts into c++
+        if self.experts_impl is None:
+            self.experts_impl = self.experts.forward
+        fused_experts_out = self.experts_impl(
+            hidden_states=hidden_states, router_logits=router_logits
+        )
+
+        # Use cached attributes instead of dynamic access
+        gate_up_proj = self.shared_experts_gate_up_proj
+        down_proj = self.shared_experts_down_proj
+        shared_experts_is_int8 = self.shared_experts_is_int8
+        shared_experts_is_fp8 = self.shared_experts_is_fp8
+        shared_experts_weight_block_size = self.shared_experts_weight_block_size
+
+        # [Note] inplace should be False in fused_experts.
+        # If inplace is True in fused_experts (self.experts), hidden_states will be changed after fused_experts
+        # While hidden_states is still needed in shared_expert.
+        final_hidden_states = self.sgl_kernel_cpu_shared_expert(
+            hidden_states,
+            gate_up_proj.weight,
+            down_proj.weight,
+            fused_experts_out,
+            self.routed_scaling_factor,
+            inplace=True,
+            use_int8_w8a8=shared_experts_is_int8,
+            use_fp8_w8a16=shared_experts_is_fp8,
+            w1_scale=(
+                gate_up_proj.weight_scale
+                if shared_experts_is_int8
+                else (gate_up_proj.weight_scale_inv if shared_experts_is_fp8 else None)
+            ),
+            w2_scale=(
+                down_proj.weight_scale
+                if shared_experts_is_int8
+                else down_proj.weight_scale_inv if shared_experts_is_fp8 else None
+            ),
+            block_size=(
+                shared_experts_weight_block_size if shared_experts_is_fp8 else None
+            ),
+        )
+
+        if self.tp_size > 1:
+            final_hidden_states = tensor_model_parallel_all_reduce(final_hidden_states)
+
+        return final_hidden_states.view(num_tokens, hidden_dim)
+
+    def forward_normal(self, hidden_states: torch.Tensor) -> torch.Tensor:
         num_tokens, hidden_dim = hidden_states.shape
         hidden_states = hidden_states.view(-1, hidden_dim)
         if self.gate_impl is None:
