94fb22f1aa343c19427446c830b9f845c0ae65fc
y <chunyuan.wu@intel.com>
2025-05-30 06:26:03 +0000

    change to  torch.ops.sgl_kernel after switching to cmake

--- python/sglang/srt/layers/moe/fused_moe_triton/layer.py
+++ python/sglang/srt/layers/moe/fused_moe_triton/layer.py
@@ -118,11 +118,11 @@ class UnquantizedFusedMoEMethod(FusedMoEMethodBase, CustomOp):
         
         import sgl_kernel
         layer.w13_weight = torch.nn.Parameter(
-            sgl_kernel.common_ops.convert_weight_packed(layer.w13_weight.data),
+            torch.ops.sgl_kernel.convert_weight_packed(layer.w13_weight.data),
             requires_grad=False,
         )
         layer.w2_weight = torch.nn.Parameter(
-            sgl_kernel.common_ops.convert_weight_packed(layer.w2_weight.data),
+            torch.ops.sgl_kernel.convert_weight_packed(layer.w2_weight.data),
             requires_grad=False,
         )        
         
