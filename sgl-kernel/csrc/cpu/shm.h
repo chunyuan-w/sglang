e835f0a56eac5ed82a2f37919ce9b05d230e0ae0
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-11-13 14:46:51 +0000

    use constexpr for state_group

--- sgl-kernel/csrc/cpu/shm.h
+++ sgl-kernel/csrc/cpu/shm.h
@@ -6,9 +6,11 @@
 #define __SHM_COLLECTIVES__
 #define VECTOR_LENGTH_IN_BYTES 32
 
-// TODO: put this in .h and let shm.cpp also use it?
+constexpr int STATE_GROUP_SYMMETRIC_ALLREDUCE = 0;
+constexpr int STATE_GROUP_DISTRIBUTED_ALLREDUCE = 1;
 constexpr int STATE_GROUP_ALL_GATHER = 2;
 constexpr int STATE_GROUP_ALL_GATHER_INTO_TENSOR = 3;
+constexpr int STATE_GROUP_REDUCE_SCATTER = 4;
 
 void shm_initialize(int size, int rank, const char* addr_string, const char* port_string);
 void all_reduce_outer_loop(torch::Tensor& data, size_t numel, int data_size);
