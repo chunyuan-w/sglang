fabe5e7b0b9e7194274f7093b189c164c2a578ac
Wu, Chunyuan <chunyuan.wu@intel.com>
2026-03-26 11:02:43 +0000

    set torch_dtype to bf16 instead of fp16 for minimax

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -1214,6 +1214,14 @@ def _get_and_verify_dtype(
                         "want to use float16."
                     )
                     torch_dtype = torch.bfloat16
+                # TODO: merge this with the above if for gemma?
+                elif model_type.startswith("minimax"):
+                    logger.info(
+                        f"For {model_type}, we downcast float32 to bfloat16 instead "
+                        "of float16 by default. Please specify `dtype` if you "
+                        "want to use float16."
+                    )
+                    torch_dtype = torch.bfloat16                    
                 else:
                     # Following the common practice, we use float16 for float32
                     # models.
